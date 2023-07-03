8c41833818d97ff3a2bdc4f0b2bc6f9f29afad96
akyrey <stein.dario@gmail.com>
2023-07-03 23:22:29 +0200

    Fix TwoCrystalBalls complexity
    We have to increase i by jmpAmount, otherwise we are not getting the
    O(sqrt(N)) complexity but the usual O(N)

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -2,7 +2,7 @@ export default function two_crystal_balls(breaks: boolean[]): number {
     const jmpAmount = Math.floor(Math.sqrt(breaks.length));
 
     let i = jmpAmount;
-    for (; i < breaks.length; i++) {
+    for (; i < breaks.length; i += jmpAmount) {
         if (breaks[i]) {
             break;
         }
